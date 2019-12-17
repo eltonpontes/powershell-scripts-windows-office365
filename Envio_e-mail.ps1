9c15bb1e0eb29cc91b5363b40197ddba8fa5af8b
Elton Pontes <contato@eltonpontes.com.br>
2019-12-17 14:22:30 -0300

    Envio de Email

---
+++ Envio_e-mail.ps1
@@ -0,0 +1,16 @@
+﻿$SMTPServer = "smtp.office365.com" 
+$SMTPPort = "587" 
+$Username = "email@dominio.com.br" 
+$Password = 'Password'
+$to = "contato@eltonpontes.com.br" 
+#$cc = "emailcopia@dominio.com.br" 
+#$bcc = "emailoculto@dominio.com.br" 
+$subject = "Assunto do E-mail" 
+$body = "Este e-mail foi enviado usando PowerShell.
+Att, 
+Sua Assinatura"
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+$smtp.EnableSSL = $true 
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password); 
+$smtp.send($message); 
+write-host "E-mail Enviado!"
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # powershell
 
-•Active Directory
+• Active Directory
 
  - Disable Users - Crie um .txt e adicione todas a contas que deseja desabilitar em massa.
  - Import User Groups - Importando usuarios em massa para dentro de um grupo do Active Directory.
  - Computers Move Groups - Movendo computadores em massa para uma OU expecifica.
 
-•Microsoft Office 365
+• Microsoft Office 365
 
  - Connect Office 365 Exchange - Conectando no Exchange Online.
+ - Envio de E-mail - enviando e-mail através do Powershell.
